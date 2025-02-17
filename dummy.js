d970deb4d0870a5201820a7d7b0629dce5906dd9
045-ayush <ayushrawat@wingmanpartners.com>
2025-02-17 16:57:27 +0530

    adding comments

--- dummy.js
+++ dummy.js
@@ -9,4 +9,5 @@ class Node {
 // Example usage to create a simple linked list
 let head = new Node(1);
 head.next = new Node(2);
-head.next.next = new Node(3); 
+head.next.next = new Node(3);
+// adding a comment
