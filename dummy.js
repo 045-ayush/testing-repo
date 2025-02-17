66eaad8d3f2eb31d766bc5fd0b677665b48ecda0
045-ayush <ayushrawat@wingmanpartners.com>
2025-02-17 16:28:27 +0530

    Create dummy.js

---
+++ dummy.js
@@ -0,0 +1,12 @@
+// Basic Node structure 
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next; 
+  }
+}
+
+// Example usage to create a simple linked list
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3); 
